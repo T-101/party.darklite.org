82ea3091fce5968455d3ed4d2b8c03b4e15c3876
T-101 <antti.rummukainen@gmail.com>
2021-04-09 03:12:14 +0300

    viewset to accept optional scene_id for creation credit

--- app/party/api/v1/viewsets.py
+++ app/party/api/v1/viewsets.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework.viewsets import ModelViewSet
 
 from party.api.v1.serializers import PartySerializer
@@ -8,3 +9,10 @@ from party.models import Party
 class PartyViewSet(ModelViewSet):
     queryset = Party.objects.all()
     serializer_class = PartySerializer
+
+    def perform_create(self, serializer):
+        scene_id = self.request.data.get("scene_id", None)
+        if scene_id:
+            serializer.save(created_by=get_user_model().objects.get(scene_id=scene_id))
+        else:
+            return super().perform_create(serializer)
