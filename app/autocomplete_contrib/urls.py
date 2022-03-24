9658515456fb6e7331e847561cebd6c620cb22fb
T-101 <antti.rummukainen@gmail.com>
2022-03-24 14:23:39 +0200

    added url config for users autocomplete

--- app/autocomplete_contrib/urls.py
+++ app/autocomplete_contrib/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from autocomplete_contrib.views import CountryAutoComplete
+from autocomplete_contrib.views import CountryAutoComplete, UserAutoComplete
 
 urlpatterns = [
     path('dal-countries/', CountryAutoComplete.as_view(), name='dal-countries'),
+    path('dal-users/', UserAutoComplete.as_view(), name='dal-users')
 ]
--- app/common/context_processors.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-
-
-def google_analytics(request):
-    return {
-        'ga_tag': settings.GOOGLE_ANALYTICS or None
-    }
