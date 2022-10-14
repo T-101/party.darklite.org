1ff1921aedab6f7508ffddf26a47ca6af9e59e3a
T-101 <antti.rummukainen@gmail.com>
2022-10-14 13:20:15 +0300

    gunicorn settings to config file

---
+++ app/gunicorn.py
@@ -0,0 +1,12 @@
+import os
+
+accesslog = "/var/log/access.log"
+bind = "0.0.0.0:" + os.environ.get("CONTAINER_PORT")
+errorlog = "/var/log/error.log"
+graceful_timeout = 3600
+keep_alive = 3600
+log_level = "debug"
+pid = "partywiki.pid"
+timeout = 3600
+threads = 2
+workers = os.environ.get("UWSGI_WORKERS")
