f9c1a513044af7e560cad4cafe807c375fd146d2
T-101 <antti.rummukainen@gmail.com>
2022-10-15 11:42:11 +0300

    get real user ip to logs, instead of docker ip

--- app/gunicorn.py
+++ app/gunicorn.py
@@ -1,14 +1,15 @@
 import os
 
+access_log_format = '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 accesslog = "/var/log/access.log"
 bind = "0.0.0.0:" + os.environ.get("CONTAINER_PORT")
+capture_output = True  # Capture stdio
+enable_stdio_inheritance = True  # Django errors from stderr to error log
 errorlog = "/var/log/error.log"
 graceful_timeout = 3600
 keep_alive = 3600
 log_level = "debug"
 pid = "partywiki.pid"
-timeout = 3600
 threads = 2
+timeout = 3600
 workers = os.environ.get("UWSGI_WORKERS")
-capture_output = True
-enable_stdio_inheritance = True
