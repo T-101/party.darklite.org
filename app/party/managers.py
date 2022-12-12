0e7e3ac9afa47fbc37b9ce143d82c96ddc6a2d10
T-101 <antti.rummukainen@gmail.com>
2022-12-12 11:39:17 +0200

    select related user data by default, reduces db hits

--- app/party/managers.py
+++ app/party/managers.py
@@ -2,11 +2,18 @@ from django.db import models
 from django.utils import timezone
 
 
+class PartyManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().select_related("created_by", "modified_by")
+
+
 class UpcomingPartyManager(models.Manager):
     def get_queryset(self):
-        return self.model.objects.filter(date_end__gte=timezone.now()).order_by('date_start')
+        return self.model.objects.select_related("created_by", "modified_by").filter(
+            date_end__gte=timezone.now()).order_by('date_start')
 
 
 class PastPartyManager(models.Manager):
     def get_queryset(self):
-        return self.model.objects.filter(date_end__lt=timezone.now()).order_by('-date_start')
+        return self.model.objects.select_related("created_by", "modified_by").filter(
+            date_end__lt=timezone.now()).order_by('-date_start')
