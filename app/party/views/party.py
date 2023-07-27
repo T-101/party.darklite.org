223175c2972f8f0da6cfd2e3c879bcc44158e278
T-101 <antti.rummukainen@gmail.com>
2023-07-27 13:19:46 +0300

    refactored code to use demoparty.net api. removed dead code

--- app/party/views/party.py
+++ app/party/views/party.py
@@ -1,5 +1,3 @@
-import feedparser
-from datetime import datetime
 import string
 
 import requests
@@ -7,17 +5,13 @@ from django.db.models.functions import Lower
 from django.contrib import messages
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.text import slugify
 from django.views import generic
-from django.conf import settings
 
 from common.mixins import LoginRequiredMixin
 from party.forms import PartyForm, PartyFormNative
 from party.models import Party
-
-
-def fetch_demopartynet_parties():
-    return feedparser.parse(settings.DEMOPARTYNET_RSS_FEED)
 
 
 class PartyListView(generic.ListView):
@@ -52,20 +46,10 @@ class PartyCreateView(LoginRequiredMixin, generic.CreateView):
             return PartyFormNative
         return PartyForm
 
-    @staticmethod
-    def _get_demoparties():
-        feed = fetch_demopartynet_parties()
-        return [(p.title, p.link, p.demopartynet_country) for p in feed.entries]
-
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         form.instance.visible = True
         return super().form_valid(form)
-
-    def get_context_data(self, **kwargs):
-        ctx = super().get_context_data(**kwargs)
-        ctx["parties"] = self._get_demoparties()
-        return ctx
 
     def get_success_url(self):
         return reverse("party:landing_page")
@@ -75,6 +59,13 @@ class PartyUpdateView(LoginRequiredMixin, generic.UpdateView):
     template_name = 'party/party_create.html'
     model = Party
     form_class = PartyForm
+
+    def get(self, request, *args, **kwargs):
+        party = self.get_object()
+        if party.has_ended:
+            messages.warning(request, "Cannot modify a party that has ended")
+            return redirect(reverse("party:landing_page"))
+        return super().get(request, *args, **kwargs)
 
     def form_valid(self, form):
         form.instance.modified_by = self.request.user
@@ -99,50 +90,35 @@ class PartyDetailView(generic.DetailView):
 
 class DemopartyNetCreateView(LoginRequiredMixin, generic.RedirectView):
     def get(self, request, *args, **kwargs):
-        demopartynet_slug = request.GET.get("slug", None)
-        if not demopartynet_slug:
-            return redirect("party:landing_page")
-        try:
-            feed = fetch_demopartynet_parties()
-        except Exception:
-            messages.warning(self.request, "Error connecting to demoparty.net")
-            return redirect("party:landing_page")
-        if Party.objects.filter(slug=slugify(demopartynet_slug)):
-            messages.info(request, "Party already exists")
-            return redirect("party:detail", slug=slugify(demopartynet_slug))
-        party_feed = [p for p in feed.entries if slugify(p.title) == slugify(demopartynet_slug)]
-        if not party_feed:
-            messages.warning(request, "Unknown party slug, unable to add party")
-            return redirect("party:landing_page")
-        if len(party_feed) != 1:
-            messages.warning(self.request, "More than one party found with slug, unable to add party")
-            return redirect("party:landing_page")
-        party_feed = party_feed[0]
+        demopartynet_id = request.GET.get("id", None)
         address = []
-        if party_feed.link:
-            url = f"{party_feed.link}.jsonld"
-            res = requests.get(url=url)
-            if res.status_code == 200:
-                j = res.json()
-                if type(j.get("location")) == dict:
-                    address.append(j.get("location", {}).get("address", {}).get("streetAddress", ""))
-                    address.append(j.get("location", {}).get("address", {}).get("postalCode", ""))
-                    address.append(j.get("location", {}).get("address", {}).get("addressLocality", ""))
-        if len(address):
-            address = [p for p in address if p != ""]
-            address = ", ".join(address)
-        else:
-            address = "Unknown"
         defaults = {
-            "name": party_feed.title,
-            "www": party_feed.demopartynet_url,
-            "date_start": datetime.strptime(party_feed.demopartynet_startdate, '%a, %d %b %Y %X %z'),
-            "date_end": datetime.strptime(party_feed.demopartynet_enddate, '%a, %d %b %Y %X %z'),
-            "location": address,
-            "country": str(party_feed.demopartynet_country).upper(),
             "created_by": request.user
         }
-        party, created = Party.objects.update_or_create(slug=slugify(demopartynet_slug), defaults=defaults)
-        if created:
-            messages.success(request, "Party created succesfully")
+
+        url = f"https://www.demoparty.net/api/events/{demopartynet_id}"
+        res = requests.get(url=url, timeout=10)
+        if res.status_code != 200:
+            messages.warning(request, "Error fetching data from demoparty.net")
+            return redirect(reverse("party:landing_page"))
+
+        j = res.json()
+        if j.get("location", {}).get("@type") == "VirtualLocation":
+            defaults["location"] = "Online"
+        else:
+            address.append(j.get("location", {}).get("address", {}).get("streetAddress", ""))
+            address.append(j.get("location", {}).get("address", {}).get("postalCode", ""))
+            address.append(j.get("location", {}).get("address", {}).get("addressLocality", ""))
+            defaults["location"] = ", ".join([p for p in address if p != ""])
+        defaults["name"] = j.get("name")
+        defaults["www"] = j.get("url")
+        try:
+            defaults["date_start"] = timezone.datetime.strptime(j.get("startDate"), "%Y-%m-%dT%H:%M:%S%z").date()
+            defaults["date_end"] = timezone.datetime.strptime(j.get("endDate"), "%Y-%m-%dT%H:%M:%S%z").date()
+        except ValueError:
+            messages.warning(request, "Error parsing dates from demoparty.net")
+            return redirect(reverse("party:landing_page"))
+        defaults["country"] = j.get("location", {}).get("address", {}).get("addressCountry", "")[-2:]
+
+        party, _ = Party.objects.update_or_create(slug=slugify(defaults["name"]), defaults=defaults)
         return redirect('party:detail', party.slug)
