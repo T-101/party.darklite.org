a984ef53f5f832efec32ec3781ee4d2bdc1c16cc
T-101 <antti.rummukainen@gmail.com>
2024-05-28 13:45:26 +0300

    added more fields so this will also validate as atom

--- app/common/feeds.py
+++ app/common/feeds.py
@@ -8,18 +8,17 @@ from party.models import Party
 
 class BasePartyFeed(Feed):
     title = "Darklite Partywiki Parties"
-    link = "https://party.darklite.org"
+    link = "https://party.darklite.org/"
     description = "You'll never travel alone - Even if you'd want to!"
 
     def get_feed(self, obj, request):
         # Since we are running behind a reverse proxy, we need to tell Django that we are running on HTTPS
-        request.is_secure = lambda: True
+        # when in production. This is because the reverse proxy is handling the SSL termination.
+        request.is_secure = lambda: not settings.DEBUG
 
         feed = super().get_feed(obj, request)
         if settings.DEBUG:
             feed.content_type = 'text/xml; charset=utf-8'
-        else:
-            feed.content_type = 'application/rss+xml; charset=utf-8'
         return feed
 
     def items(self):
@@ -28,11 +27,17 @@ class BasePartyFeed(Feed):
     def item_title(self, item):
         return item
 
+    def author_name(self):
+        return "Darklite Partywiki"
+
     def item_description(self, item):
         return f"{item.date_range} - {item.location}, {item.country.name}"
 
     def item_pubdate(self, item):
         return item.created_dt
+
+    def item_updateddate(self, item):
+        return item.modified_dt
 
     def item_link(self, item):
         return reverse('party:detail', args=[item.slug])
