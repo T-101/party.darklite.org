3773c7026e74edc56895c0793bc63646d7eaa2ff
T-101 <antti.rummukainen@gmail.com>
2025-01-06 11:50:48 +0200

    simplify form

--- app/authentication/forms.py
+++ app/authentication/forms.py
@@ -1,27 +1,15 @@
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit, Div
 from django.contrib.auth import get_user_model
-from django.forms import ModelForm
+from django.forms import ModelForm, CharField, TextInput
 
 
 class DisplayNameForm(ModelForm):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.layout = Layout(
-            Div(
-                'display_name'
-            ),
-            ButtonHolder(
-                Submit('submit', 'Change')
-            )
-        )
+    display_name = CharField(
+        label="Your name",
+        widget=TextInput(attrs={"placeholder": "Your name", "class": "form-control", "data-1p-ignore": "true"}),
+        help_text="This is the default display name that will be written to a new trip. "
+                  "Changing this will not affect previous trips."
+    )
 
     class Meta:
         model = get_user_model()
         fields = ['display_name']
-        help_texts = {
-            "display_name": "This is the default display name that will be written to a new trip. "
-                            "Changing this will not affect previous trips."
-        }
