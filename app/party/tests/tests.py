e726a819e67e7232c3281228f4daa8b223e537bd
T-101 <antti.rummukainen@gmail.com>
2025-04-30 21:21:14 +0300

    fixed tests

--- app/party/tests/tests.py
+++ app/party/tests/tests.py
@@ -9,7 +9,7 @@ from party.parsers import get_location, get_country, get_start_date, get_end_dat
 
 class ParserTest(TestCase):
     def setUp(self):
-        files = ["inercia2023.json", "birdie2024.json"]
+        files = ["inercia2023.json", "birdie2024.json", "graffathon2025.json"]
         self.payloads = []
         for file in files:
             filepath = os.path.join(settings.BASE_DIR, "party/tests", file)
@@ -17,18 +17,19 @@ class ParserTest(TestCase):
                 self.payloads.append(json.load(f))
 
     def test_location_parser(self):
-        self.assertEqual(get_location(self.payloads[0]), "Unknown")
+        self.assertEqual(get_location(self.payloads[0]), "Rua Sociedade Filarmónica Incrível Almadense 8A, 2800-158, Almada")
 
-        self.assertEqual(get_location(self.payloads[1]), "Uppsala, Sweden, Uppsala")
+        self.assertEqual(get_location(self.payloads[1]), "Online")
 
     def test_country_parser(self):
         self.assertEqual(get_country(self.payloads[0]), "PT")
         self.payloads[0]["location"] = None
-        self.assertEqual(get_country(self.payloads[0]), "PT")
+        self.assertEqual(get_country(self.payloads[0]), '')
         self.payloads[0]["locationCountry"] = None
         self.assertIsNone(get_country(self.payloads[0]))
 
-        self.assertEqual(get_country(self.payloads[1]), "SE")
+        self.assertEqual(get_country(self.payloads[1]), "")
+        self.assertEqual(get_country(self.payloads[2]), "FI")
 
     def test_date_parser(self):
         self.assertEqual(get_start_date(self.payloads[0]).strftime("%Y-%m-%d"), "2023-12-01")
@@ -36,7 +37,7 @@ class ParserTest(TestCase):
         self.payloads[0]["startDate"] = None
         self.assertEqual(get_start_date(self.payloads[0]).strftime("%Y-%m-%d"), "1970-01-01")
 
-        self.assertEqual(get_start_date(self.payloads[1]).strftime("%Y-%m-%d"), "2024-05-09")
+        self.assertEqual(get_start_date(self.payloads[1]).strftime("%Y-%m-%d"), "2024-05-11")
         self.assertEqual(get_end_date(self.payloads[1]).strftime("%Y-%m-%d"), "2024-05-12")
         self.payloads[0]["endDate"] = None
         self.assertEqual(get_end_date(self.payloads[0]).strftime("%Y-%m-%d"), "1970-01-01")
